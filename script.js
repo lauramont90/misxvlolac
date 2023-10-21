337a297a07493ff106eb9175411006ec00ed9f84
María Laura Montiel <laumontiel1990@gmail.com>
2023-10-21 17:46:04 -0300

    add sections

--- script.js
+++ script.js
@@ -25,3 +25,20 @@ setInterval(updateCountdown, 1000);
 
 // Llama a la función para actualizar el contador inmediatamente
 updateCountdown();
+// Obtén una referencia al botón por su ID
+const botonWhatsapp = document.getElementById("botonWhatsapp");
+const botonGoogleMaps = document.getElementById("botonGoogleMaps");
+
+// Agrega un manejador de eventos al botón
+botonWhatsapp.addEventListener("click", function() {
+    // URL de WhatsApp que deseas abrir
+    const urlWhatsapp = "https://wa.me/+541124683994?text=Mi%20nombre%20y%20apellido%20es...%20Gracias%20por%20la%20invitación";
+
+    // Redirige a la URL de WhatsApp
+    window.open(urlWhatsapp, "_blank");
+});
+// Agrega un manejador de eventos al botón de Google Maps
+botonGoogleMaps.addEventListener("click", function() {
+    const urlGoogleMaps = "https://maps.app.goo.gl/RUQuJaD7oojoVfWX6";
+    window.open(urlGoogleMaps, "_blank");
+});
